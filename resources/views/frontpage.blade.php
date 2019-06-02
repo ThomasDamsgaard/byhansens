c58a419aa1f72aac0c064d2a22842f969cdbc1bc
vagrant <vagrant@homestead>
2019-06-02 18:10:13 +0000

    Added pictures

--- resources/views/frontpage.blade.php
+++ resources/views/frontpage.blade.php
@@ -16,20 +16,23 @@
 				<!-- Header -->
 					<header id="header">
 						{{-- <div class="logo">
-							<img src="{{ asset('storage/img/logo.png') }}" alt="">
+							<img src="{{ asset('storage/img/logo.png') }}" alt="" style="width: 100%; height: auto;">
 						</div> --}}
 						<div class="content">
 							<div class="inner">
-								<h1>By Hansens</h1>
+								{{-- <h1>By Hansens</h1> --}}
+								<img src="{{ asset('storage/img/logo.png') }}" alt="" class="logo-responsive">
+
 								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit <br> sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
 							</div>
 						</div>
 						<nav>
 							<ul>
-								<li><a href="#intro">Chokolade</a></li>
-								<li><a href="#work">Filosofi</a></li>
-								<li><a href="#about">Om os</a></li>
-								<li><a href="#contact">Kontakt</a></li>
+								<li><a href="#chokolade">Chokolade</a></li>
+								<li><a href="#ravarer">Råvarer</a></li>
+								<li><a href="#produkter">Produkter</a></li>
+								<li><a href="#om">Om os</a></li>
+								<li><a href="#kontakt">Kontakt</a></li>
 							</ul>
 						</nav>
 					</header>
@@ -37,36 +40,52 @@
 				<!-- Main -->
 					<div id="main">
 
-						<!-- Intro -->
-							<article id="intro">
+						<!-- Chokolade -->
+							<article id="chokolade">
 								<h2 class="major">Chokolade</h2>
-								<span class="image main"><img src="{{ asset('assets/img/pic01.jpg') }}" alt="" /></span>
-								<p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#work">awesome work</a>.</p>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
+								<span class="image main"><img src="{{ asset('assets/img/image00012.jpeg') }}" alt="" style="max-width: 300px;"/></span>
+								<p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#råvarer">råvarer</a>.</p>
 							</article>
+						<!-- Chokolade -->
 
-						<!-- Work -->
-							<article id="work">
-								<h2 class="major">Filosofi</h2>
-								<span class="image main"><img src="{{ asset('assets/img/pic02.jpg') }}" alt="" /></span>
+						<!-- Råvarer -->
+							<article id="ravarer">
+								<h2 class="major">Råvarer</h2>
+								<span class="image main"><img src="{{ asset('assets/img/image00021.jpeg') }}" alt="" style="max-width: 300px;"/></span>
 								<p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
 								<p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
 							</article>
+						<!-- Råvarer -->
 
-						<!-- About -->
-							<article id="about">
+						<!-- Produkter -->
+							<article id="produkter">
+								<h2 class="major">Produkter</h2>
+								<span class="image main"><img src="{{ asset('assets/img/image00016.jpeg') }}" alt="" style="max-width: 300px;"/></span>
+								<p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
+								<p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
+							</article>
+						<!-- Produkter -->
+
+						<!-- Om -->
+							<article id="om">
 								<h2 class="major">Om os</h2>
-								<span class="image main"><img src="{{ asset('assets/img/pic03.jpg') }}" alt="" /></span>
+								<span class="image main"><img src="{{ asset('assets/img/image00017.jpeg') }}" alt="" style="max-width: 300px;"/></span>
 								<p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
 							</article>
+						<!-- Om -->
 
-						<!-- Contact -->
-							<article id="contact">
+						<!-- Kontakt -->
+							<article id="kontakt">
 								<h2 class="major">Kontakt</h2>
+								<div class="">
+									<img src="{{ asset('storage/img/logo.png') }}" alt="" class="logo-responsive">
+
+								</div>
+
 								<form method="post" action="#">
 									<div class="fields">
 										<div class="field half">
-											<label for="name">Name</label>
+											<label for="name">Navn</label>
 											<input type="text" name="name" id="name" />
 										</div>
 										<div class="field half">
@@ -74,13 +93,13 @@
 											<input type="text" name="email" id="email" />
 										</div>
 										<div class="field">
-											<label for="message">Message</label>
+											<label for="message">Besked</label>
 											<textarea name="message" id="message" rows="4"></textarea>
 										</div>
 									</div>
 									<ul class="actions">
-										<li><input type="submit" value="Send Message" class="primary" /></li>
-										<li><input type="reset" value="Reset" /></li>
+										<li><input type="submit" value="Send Besked" class="primary" /></li>
+										<li><input type="reset" value="Nulstil" /></li>
 									</ul>
 								</form>
 								<ul class="icons">
@@ -89,8 +108,10 @@
 									<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
 								</ul>
 							</article>
+							<!-- Kontakt -->
 
 					</div>
+					<!-- Main -->
 
 				<!-- Footer -->
 					<footer id="footer">
