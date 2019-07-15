719767ef5ca5879a40629d2f7e5d42cab072b38d
vagrant <vagrant@homestead>
2019-07-15 19:17:21 +0000

    Finished v.1

--- resources/views/frontpage.blade.php
+++ resources/views/frontpage.blade.php
@@ -23,15 +23,17 @@
 								{{-- <h1>By Hansens</h1> --}}
 								<img src="{{ asset('assets/img/logo-hvid.png') }}" alt="" class="logo-responsive" style="opacity: 0.7;">
 
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit <br> sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+								<p>
+									Velkommen til en verden af søde og smagfulde snacks fyldt med gode og nærende råvarer.<br>
+									Her er alt lavet på naturlige ingredienser, der er til at forstå.
+								</p>
+
 							</div>
 						</div>
 						<nav>
 							<ul>
-								<li><a href="#chokolade">Chokolade</a></li>
-								<li><a href="#ravarer">Råvarer</a></li>
-								<li><a href="#produkter">Produkter</a></li>
-								<li><a href="#om">Om os</a></li>
+								<li><a href="#raavarer">Råvarer</a></li>
+								<li><a href="#om-os">Om os</a></li>
 								<li><a href="#kontakt">Kontakt</a></li>
 							</ul>
 						</nav>
@@ -40,116 +42,129 @@
 				<!-- Main -->
 					<div id="main">
 
-						<!-- Chokolade -->
-							<article id="chokolade">
-								<h2 class="major">Chokolade</h2>
-								<span class="image main"><img src="{{ asset('assets/img/image00012.jpeg') }}" alt="" style="max-width: 300px;"/></span>
-								<p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#råvarer">råvarer</a>.</p>
-							</article>
-						<!-- Chokolade -->
-
 						<!-- Råvarer -->
-							<article id="ravarer">
-								<h2 class="major">Råvarer</h2>
+							<article id="raavarer">
+								<h2 class="major">Kun naturlige råvarer</h2>
 								<span class="image main"><img src="{{ asset('assets/img/image00021.jpeg') }}" alt="" style="max-width: 300px;"/></span>
-								<p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-								<p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
+								<p><span><b><i>Vi</i></b></span> har fyldt vores produkter med de allerbedste råvarer, for at give dig det allerbedste resultat. Her kan du læse om alle vores råvarer; hvorfor vi har valgt dem, og hvorfor de er gode for dig.</p>
+								<p>
+									<span><b><i>Kikærter</i></b></span> er en fantastisk råvare med mange gode egenskaber – og de bliver kun bedre af at blive pakket ind i 70% mørk luksuschokolade! Sammen med en masse andre gode råvarer udgør kikærterne selve grundpillen i vores Chickpea Cubes. Kikærter har et højt indhold af både protein og fibre, og så er de fyldt med A, B og E-vitaminer såvel som kalium, calcium, magnesium og jern. Derfor smager vores Chickpea Cubes ikke bare fantastisk – de er faktisk også et godt supplement til din kost.
+								</p>
+								<p>
+									<span><b><i>Peanutbutter</i></b></span> er den anden af de to bærende ingredienser i vores Chickpea Cubes, og dét er den af mange gode grunde. Først og fremmest er den fede jordnøddesmør en overset kilde til sundere fedttyper i vores hverdag. Den cremede masse har desuden også et højt indhold af protein og fibre – og så smager den selvfølgelig bare hamrende godt. Derfor har vi valgt peanutbutter som limen, der holder sammen på alle vores gode råvarer.
+								</p>
+								<p>
+									<span><b><i>Spelt</i></b></span> er populært som aldrig før, og med god grund. For selvom den klassiske hvedesort har mere end 7.000 år på bagen og dermed er én af de ældste, der findes, så har den stadig enormt gavnlige egenskaber i forhold til vores moderne livsstil. Speltflagerne i vores Chickpea Cubes bidrager med et højt indhold af fibre og proteiner, og så giver de stykkerne et dejligt bid.
+								</p>
+								<p>
+									<span><b><i>Mandler</i></b></span> har været en populær snack i årevis, så lige fra starten var det oplagt for os at bruge den lille vidundernød. Også her får vores Chickpea Cubes tilført et højt indhold af proteiner og fibre, og så er mandlerne med til at give smagen sit helt særlige særpræg.
+								</p>
+								<p>
+									<span><b><i>Kardemomme</i></b></span> har en helt enestående smag, som med det samme lader varmen og hyggen sprede sig. Mange af os nyder smagen, når vi mæsker os i varme hveder til St. Bededag, og et år slog det os: Den smag skal vi bruge! Derfor har vi tilsat en anelse kardemomme til vores Chickpea Cubes, for at runde smagen af og give dem en aroma af velbehag.
+								</p>
+								<p>
+									<span><b><i>Salt</i></b></span> hører til i al god madlavning, og også i vores Chickpea Cubes. Med bare en knivspids af det allestedsnærværende mineral forstærkes det samlede smagsindtryk af vores råvarer, så smagen løfter sig til et helt nyt niveau.
+								</p>
+								<p>
+									<span><b><i>Salt</i></b></span> er et lille vidundermiddel fra den canadiske natur. Det naturlige sødemiddel bliver kogt ind til en lind sirup, som delvist giver en velafbalanceret sødme til vores masse og samtidig virker konserverende, så vi ikke behøver tilsætte kunstige konserveringsmidler.
+								</p>
+								<p>
+									<span><b><i>Kokosolie</i></b></span> er med til at give vores Chickpea Cubes deres bløde struktur, idet den smeltede olie nemt fordeles og effektivt holder sammen på alle råvarerne, efterhånden som massen får lov til at køle af.
+								</p>
+								<p>
+									<span><b><i>Mørk chokolade</i></b></span> må være en lille bid af himmerige! Vi bruger chokolade med 70,5% kakao – fyldt med gode egenskaber som vitaminer, mineraler og antioxidanter. Og så går smagen helt eminent sammen med resten af vores råvarer for at skabe en fuldendt nydelse fyldt med nærende indhold.
+								</p>
 							</article>
 						<!-- Råvarer -->
-
-						<!-- Produkter -->
-							<article id="produkter">
-								<h2 class="major">Produkter</h2>
-								<span class="image main"><img src="{{ asset('assets/img/image00016.jpeg') }}" alt="" style="max-width: 300px;"/></span>
-								<p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-								<p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
-							</article>
-						<!-- Produkter -->
 
 						<!-- Om -->
-							<article id="om">
+							<article id="om-os">
 								<h2 class="major">Om os</h2>
 								<span class="image main"><img src="{{ asset('assets/img/image00003.jpeg') }}" alt="" style="max-width: 300px;"/></span>
-								<p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
+								<p>By Hansens har en vision om at revolutionere markedet og udbrede budskabet om, at slik sagtens kan være fyldt med sunde, nærende og naturlige ingredienser, uden at de behøver at smage hverken fornuftigt eller kedeligt.</p>
+								<p>Det hele startede mellem Benjamin og Martin, som begge i lang tid havde gået med en drøm om at starte egen virksomhed, hvor de havde helt frie tøjler til at føre deres drømme ud i livet. Det skulle vise sig, at de to var hinandens perfekte samarbejdspartnere, så der gik ikke længe, før By Hansens blev til. Og så tog tingene fart – troede de.</p>
+								<p>For den danske fødevarebranche er en kompliceret størrelse fyldt med reguleringer og lovgivning, som det kan være svært at finde hverken hoved eller hale i. De to måtte have hjælp og indgik derfor hurtigt samarbejde med Michael Larsen, som står bag flødebollerne fra ”Ærlig”. Han havde den indsigt i branchen, som de andre manglede, og på den måde blev to til tre. Og den idérige trio er nu klar til at vende hele branchen på hovedet!</p>
+								<p><span><b><i>Benjamin Jack Hansen</i></b></span> er uddannet klejnsmed og har efterfølgende videreuddannet sig til produktionsteknolog inden for metalindustrien. I dag arbejder han primært med udviklingen af maskiner til fødevareproduktion.</p>
+								<p><span><b><i>Martin Hansen</i></b></span> er uddannet bager og har efterfølgende videreuddannet sig til fødevareteknolog. I dag arbejder han med udvikling af fødevarer indenfor bageribranchen.</p>
 							</article>
 						<!-- Om -->
 
 						<!-- Kontakt -->
 							<article id="kontakt">
 								<h2 class="major">Kontakt</h2>
+								<p>Vi arbejder hele tiden på at udvikle os og tager derfor meget gerne imod både ris og ros såvel som nye idéer.</p>
 								<span class="image main"><img src="{{ asset('assets/img/image00017.jpeg') }}" alt="" style="max-width: 400px;"/></span>
+							
+									<hr>
 
-								{{-- <div style="display:flex; justify-content: center">
-
-									<div class="">
-										<p>
-											By Hansens Chokolade
-											<br>
-											Hansensvej 8
-											<br>
-											5000 Odense C
-										</p>
-
-									</div>
-								</div> --}}
-
-
-									<div style="display:flex; justify-content: space-evenly">
-										<div>
-											<p>
-												Martin Hansen
-												<br>
-												Stifter
-												<br>
-												Produktansvarlig
-												<br>
-												<a href="mailto:martin@byhansens.com">Mail</a>
-											</p>
+										<div class="staff">
+											<div>
+												<p style="margin: 0;">
+													Martin Hansen
+													<br>
+													Stifter
+													<br>
+													Produktansvarlig
+												</p>
+											</div>
+											<div>
+												<ul class="icons">
+													<li><a href="mailto:martin@byhansens.com" class="icon fa-at"><span class="label">Twitter</span></a></li>
+													<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+													<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+												</ul>
+											</div>
 										</div>
-										<div>
-											<p>
-												Benjamin Hansen
-												<br>
-												Stifter
-												<br>
-												Salgsansvarlig
-												<br>
-												<a href="mailto:benjamin@byhansens.com">Mail</a>
-											</p>
+
+										<hr>
+
+										<div class="staff">
+											<div>
+												<p style="margin: 0;">
+													Benjamin Hansen
+													<br>
+													Stifter
+													<br>
+													Salgsansvarlig
+												</p>
+											</div>
+											<div>
+												<ul class="icons">
+													<li><a href="mailto:benjamin@byhansens.com" class="icon fa-at"><span class="label">Twitter</span></a></li>
+													<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+													<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+												</ul>
+											</div>
 										</div>
-									</div>
 
+										<hr>
 
-
-
-								{{-- <form method="POST" action="{{ route('contact.send') }}">
-									@csrf
-									<div class="fields">
-										<div class="field half">
-											<label for="name">Navn</label>
-											<input type="text" name="name" id="name" />
+										<div class="staff">
+											<div>
+												<p style="margin: 0;">
+													Michael Larsen
+													<br>
+													Stifter
+													<br>
+													Kvalitetsansvarlig
+												</p>
+											</div>
+											<div>
+												<ul class="icons">
+													<li><a href="mailto:michael@byhansens.com" class="icon fa-at"><span class="label">Twitter</span></a></li>
+													<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+													<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+												</ul>
+											</div>
 										</div>
-										<div class="field half">
-											<label for="email">Email</label>
-											<input type="text" name="email" id="email" />
-										</div>
-										<div class="field">
-											<label for="message">Besked</label>
-											<textarea name="message" id="message" rows="4"></textarea>
-											@captcha
-										</div>
-									</div>
-									<ul class="actions">
-										<li><input type="submit" value="Send Besked" class="primary" /></li>
-										<li><input type="reset" value="Nulstil" /></li>
-									</ul>
-								</form> --}}
 
-								<ul class="icons">
+										<hr>
+
+								{{-- <ul class="icons">
 									<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
 									<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
 									<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-								</ul>
+								</ul> --}}
 							</article>
 							<!-- Kontakt -->
 
