d9046d8d79da5f3d8b841fa27ee0f58eb3f772e1
ThomasDamsgaard <thomas.damsgaard@hotmail.com>
2020-02-26 09:07:03 +0000

    Vendors update 1.2

--- resources/views/frontpage.blade.php
+++ resources/views/frontpage.blade.php
@@ -231,6 +231,7 @@
 								<p>Du kan støde på vores Chickpea Cubes disse steder.</p>
 								<p><span><b><i>Analog</i></b></span><br>Albanigade 54c, Odense C.</p>
 								<p><span><b><i>The Food Stall - Magasin</i></b></span><br>Vestergade 20, Odense C</p>
+								<p><span><b><i>The Food Stall</i></b></span><br>Østre Stationsvej 27, Odense C</p>
 								<p><span><b><i>Marys Coffee & Sandwich</i></b></span><br>Østre Stationsvej 6, Odense C</p>
 								<p><span><b><i>Let's Be Frank - Arkaden</i></b></span><br>Vestergade 68, Odense C</p>
 								<p><span><b><i>Den Lille Smalle</i></b></span><br>Vestergade 5, Odense C</p>
